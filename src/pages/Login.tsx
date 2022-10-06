335289eea0691acaa49a78458b05eca79abfaf34
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-10-06 09:08:37 +0100

    implement additional features for web app

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,7 +20,7 @@ export default function Login() {
 	const handleSubmit = async () => {
 		setError(false);
 		try {
-			const requestToken = await API.getRequestToken();
+			// const requestToken = await API.getRequestToken();
 			const sessionId = await API.authenticate(
 				requestToken,
 				username,
