4217ab2764059b7709c29ce9d6612e81cd945862
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-11 06:41:11 +0100

    implemented additional features for web app

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,7 @@ import LoadMoreButton from "../components/LoadMoreButton/LoadMoreButton";
 import { Wrapper } from "./Login.Styles";
 
 export default function Login() {
-	// const [username, setUsername] = useState("");
+	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [error, setError] = useState(false);
 
