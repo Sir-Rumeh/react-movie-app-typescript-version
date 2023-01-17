60f96d68398937be825286fd6eff9e95afedcb17
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-17 20:58:20 +0100

    revamp typescript code

--- src/API.ts
+++ src/API.ts
@@ -18,10 +18,10 @@ const defaultConfig = {
 // Types
 
 export type Movie = {
-	// backdrop_path: string;
-	// id: number;
-	// original_title: string;
-	// overview: string;
+	backdrop_path: string;
+	id: number;
+	original_title: string;
+	overview: string;
 	popularity: number;
 	poster_path: string;
 	title: string;
