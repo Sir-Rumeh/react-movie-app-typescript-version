7676e1b19d8bf60073a921f4affd04bc665fe464
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-21 21:00:07 +0100

    revamp typescript code

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@
 export const calcTime = (time: number): string => {
 	// const hours: number = Math.floor(time / 60);
 	// const mins: number = time % 60;
-	// return `${hours}h ${mins}m`;
+	return `${hours}h ${mins}m`;
 };
 // Convert a number to money formatting
 export const convertMoney = (money: number): string => {
