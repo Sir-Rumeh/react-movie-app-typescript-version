295c4c170768790bdc902ce11493aac548ece6ec
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-21 20:58:18 +0100

    revamp typescript code

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 // Convert time to hours and minutes
 export const calcTime = (time: number): string => {
-	const hours: number = Math.floor(time / 60);
+	// const hours: number = Math.floor(time / 60);
 	const mins: number = time % 60;
 	return `${hours}h ${mins}m`;
 };
