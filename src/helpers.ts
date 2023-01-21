fa802251d44299474fb96969584e20d8af49db3f
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-21 20:59:20 +0100

    revamp typescript code

--- src/helpers.ts
+++ src/helpers.ts
@@ -9,7 +9,7 @@ export const convertMoney = (money: number): string => {
 	const formatter: Intl.NumberFormat = new Intl.NumberFormat("en-US", {
 		// style: "currency",
 		// currency: "USD",
-		minimumFractionDigits: 0,
+		// minimumFractionDigits: 0,
 	});
 	return formatter.format(money);
 };
