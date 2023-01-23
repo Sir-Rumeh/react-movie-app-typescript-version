f76c21ae8743dc0ab3a90cbac44be90c444b9ac8
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-23 10:25:20 +0100

    implemented additional features for web app

--- src/components/BreadCrumb/BreadCrumb.tsx
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 // Styles
 import { Wrapper, Content } from "./BreadCrumb.Styles";
 // Types
-type Props = {
-	// movieTitle: string;
-};
+// type Props = {
+// 	// movieTitle: string;
+// };
 
 const BreadCrumb: React.FC<Props> = ({ movieTitle }) => {
 	return (
