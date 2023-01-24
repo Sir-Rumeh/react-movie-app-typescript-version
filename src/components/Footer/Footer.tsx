4311dc06e457a7343ca40a089402063cd28de9c7
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-24 13:42:53 +0100

    footer alignment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
 	};
 	return (
 		<section
-			// style={background}
-			className="p-5 text-center text-white position-relative"
+		// style={background}
+		// className="p-5 text-center text-white position-relative"
 		>
 			<div className="container">
 				<p className="lead">Sir_Rumeh &copy; 2022</p>
