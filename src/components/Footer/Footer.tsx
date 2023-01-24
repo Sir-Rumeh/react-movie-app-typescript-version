7ace106a04a5c19754d642204e7ec8b1d0e958d2
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-24 13:44:02 +0100

    footer alignment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
 			className="p-5 text-center text-white position-relative"
 		>
 			<div className="container">
-				{/* <p className="lead">Sir_Rumeh &copy; 2022</p> */}
+				<p className="lead">Sir_Rumeh &copy; 2022</p>
 				<a
 				// href="#header"
 				// className="position-absolute bottom-0 end-0 h1 p-5 footer"
