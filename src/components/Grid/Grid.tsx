8c85642afa8731009cb0b5b5a0f833042c19b4e0
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-25 10:29:18 +0100

    set grid block

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 const Grid: React.FC<Props> = ({ header, children }) => {
 	return (
-		// <Wrapper>
-		// 	{/* <h1>{header}</h1> */}
-		// 	{/* <Content>{children}</Content> */}
-		// </Wrapper>
+		<Wrapper>
+			{/* <h1>{header}</h1> */}
+			{/* <Content>{children}</Content> */}
+		</Wrapper>
 	);
 };
 export default Grid;
