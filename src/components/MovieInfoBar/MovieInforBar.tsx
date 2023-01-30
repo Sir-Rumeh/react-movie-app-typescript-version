4f732c21677252b9e833022dfb775edd84f77c82
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-30 07:32:27 +0100

    restructure Info bar

--- src/components/MovieInfoBar/MovieInforBar.tsx
+++ src/components/MovieInfoBar/MovieInforBar.tsx
@@ -16,7 +16,7 @@ const MovieInfoBar: React.FC<Props> = ({ time, budget, revenue }) => {
 		<Wrapper>
 			<Content>
 				<div className="column">
-					{/* <p>Running time: {calcTime(time)}</p> */}
+					<p>Running time: {calcTime(time)}</p>
 				</div>
 				<div className="column">
 					<p>Budget: {convertMoney(budget)}</p>
