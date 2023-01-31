5a047c8cf7dddd2403ebb7bf70345e56229a4bc8
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-31 21:05:48 +0100

    set search bar state

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 const SearchBar: React.FC<Props> = ({ setSearchTerm }) => {
-	const [state, setState] = useState("");
+	// const [state, setState] = useState("");
 	const initial = useRef(true);
 
 	useEffect(() => {
