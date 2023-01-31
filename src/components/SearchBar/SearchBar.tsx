fccdf8f4b86338721aff1fc0df769bd9b3668710
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-31 21:06:33 +0100

    set search bar state

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 // import searchIcon from "../../images/search-icon.svg";
 // Styles
-import { Wrapper, Content } from "./SearchBar.Styles";
+// import { Wrapper, Content } from "./SearchBar.Styles";
 
 // Types
 type Props = {
