3d95b9e20b92ee13345750e894c6f177e55e16ad
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-02-19 10:11:20 +0100

    fetch login api and set user session

--- src/API.ts
+++ src/API.ts
@@ -65,10 +65,10 @@ const apiSettings = {
 			: `${POPULAR_BASE_URL}&page=${page}`;
 		return await (await fetch(endpoint)).json();
 	},
-	// fetchMovie: async (movieId: string): Promise<Movie> => {
-	// 	const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
-	// 	return await (await fetch(endpoint)).json();
-	// },
+	fetchMovie: async (movieId: string): Promise<Movie> => {
+		const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+		return await (await fetch(endpoint)).json();
+	},
 	// fetchCredits: async (movieId: string): Promise<Credits> => {
 	// 	const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
 	// 	return await (await fetch(creditsEndpoint)).json();
