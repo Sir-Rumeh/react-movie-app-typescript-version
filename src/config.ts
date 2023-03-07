d2bd2af94f8466bc5117ff88f25a6c66ac2ad38d
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-03-07 19:40:44 +0100

    fetching movie state

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@
 // LOGGING IN AND VOTING SYSTEM
 // const REQUEST_TOKEN_URL: string = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
 // const LOGIN_URL: string = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
-const SESSION_ID_URL: string = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
+// const SESSION_ID_URL: string = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
 
 const IMAGE_BASE_URL: string = "http://image.tmdb.org/t/p/";
 // Sizes: w300, w780, w1280, original
