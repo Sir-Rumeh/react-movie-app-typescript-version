c233ec2589abd073fa891232bafe8a7bce9f72cd
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-03-07 19:39:43 +0100

    fetching movie state

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // variablle configuration for TMDB API
 
 // const API_URL: string = "https://api.themoviedb.org/3/";
-const API_KEY: string = "2de4e74c26beea138f11a1f2ccc19234";
+// const API_KEY: string = "2de4e74c26beea138f11a1f2ccc19234";
 
 const SEARCH_BASE_URL: string = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL: string = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
